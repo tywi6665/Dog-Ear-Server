54458a76e0c8f82ae8347d20c5ee3c84c000a71c
tywi6665 <tyletcole@gmail.com>
2020-11-08 17:37:38 -0700

    updating server.js

--- server.js
+++ server.js
@@ -16,38 +16,41 @@ const socketServer = http.createServer(app);
 
 const io = socketIo(socketServer);
 
+const defaultData = data = {
+    title: "",
+    imgSrc: "",
+    author: "",
+    description: "",
+    tags: []
+}
+
 io.on("connection", (socket) => {
     console.log("New client connected");
 
     socket.on('from_client', url => {
 
-        try {
-            const scrape = new Promise((resolve, reject) => {
+
+        const scrape = new Promise((resolve, reject) => {
+
+            try {
                 //Start the browser and create a browser instance
                 let browserInstance = browserObject.startBrowser();
                 // Pass the browser instance to the scraper controller
-                // if (browserInstance) {
-                resolve(scraperController(browserInstance, url))
-                // } else {
-                //     let data = {
-                //         title: "",
-                //         imgSrc: "",
-                //         author: "",
-                //         description: "",
-                //         tags: []
-                //     }
-                //     socket.emit("from_server", data);
-                // }
-            })
+                if (browserInstance) {
+                    resolve(scraperController(browserInstance, url))
+                } else {
+                    reject(socket.emit("from_server", defaultData))
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        })
 
-            scrape.then(function (data) {
-                console.log("emitting data...")
-                //emit scraped data
-                socket.emit("from_server", data);
-            })
-        } catch (error) {
-            console.error(error)
-        }
+        scrape.then(function (data) {
+            console.log("emitting data...")
+            //emit scraped data
+            socket.emit("from_server", data);
+        })
 
     });
     socket.on("disconnect", () => {
